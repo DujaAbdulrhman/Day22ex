981bd5403681cd7ba56642bb298df71c80904901
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-03-11 10:35:40 +0300

    Update projectController.java

--- src/main/java/com/example/val/Controller/projectController.java
+++ src/main/java/com/example/val/Controller/projectController.java
@@ -2,6 +2,7 @@ package com.example.val.Controller;
 import com.example.val.Api.ApiResponse;
 import com.example.val.Model.projectModel;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -26,44 +27,48 @@ public class projectController {
     @PostMapping("/update/{index}")
     public ApiResponse updateProject(@PathVariable int index, @Valid @RequestBody projectModel project) {
         projects.set(index, project);
-        return new ApiResponse("Successfully updated project");
+        return ResponseEntity.status(200).body(new ApiResponse("Successfully updated project")).getBody();
     }
 
     @DeleteMapping("/delete/{index}")
     public ApiResponse deleteProject(@PathVariable int index) {
         projects.remove(index);
-        return new ApiResponse("Successfully deleted project");
+        return ResponseEntity.status(200).body(new ApiResponse("deleted Successfully")).getBody();
+
     }
 
     @PutMapping("/updates")
-    public String setstatus(@RequestBody String status) {
+    public ResponseEntity setstatus(@RequestBody String status) {
         if (status.equalsIgnoreCase("done")) {
-            return "Project not done";
+            return ResponseEntity.status(200).body("Successfully updated project");
         } else if (status.equalsIgnoreCase("not done")) {
-            return "Project done";
+            return ResponseEntity.status(200).body("Successfully updated project");
         }
-        return status;
+        return null;
     }
 
     @GetMapping("/searchtitle")
-    public ArrayList<projectModel> searchProjectByTitle(@RequestParam String title) {
+    public ResponseEntity searchProjectByTitle(@RequestParam String title) {
         ArrayList<projectModel> foundProjects = new ArrayList<>();
         for (projectModel proj : projects) {
             if (proj.getTitle().equalsIgnoreCase(title)) {
-                foundProjects.add(proj);
-            }
+               // foundProjects.add(proj);
+                return ResponseEntity.status(200).body(foundProjects.add(proj));
+            }else return ResponseEntity.status(404).body("Not Found");
         }
-        return foundProjects;
+        return null;
     }
 
     @GetMapping("/getByCompany")
-    public ArrayList<projectModel> getProjectsByCompany(@RequestParam String companyName) {
+    public ResponseEntity<ApiResponse> getProjectsByCompany(@RequestParam String companyName) {
         ArrayList<projectModel> projByCompany = new ArrayList<>();
         for (projectModel proj : projects) {
             if (proj.getCompanyName().equalsIgnoreCase(companyName)) {
                 projByCompany.add(proj);
-            }
+                return ResponseEntity.status(200).body(new ApiResponse("project by company found"));
+
+            }else return ResponseEntity.status(404).body(new ApiResponse("project by company not found"));
         }
-        return projByCompany;
+        return null;
     }
 }
